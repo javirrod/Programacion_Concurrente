f5204b31e486d88f265f7f5cafe59cb799995af7
javirrod <jvrrdrgz@myuax.com>
2022-12-11 19:35:05 +0100

    creacion de feedback 1_2

---
+++ Feedback1_2/src/Feedback1_2/TareaDeHilo.java
@@ -0,0 +1,15 @@
+package Feedback1_2;
+
+public class TareaDeHilo implements Runnable{
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(Thread.currentThread().getName());
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
---
+++ Feedback1_2/src/Feedback1_2/TareaDeHiloLarga.java
@@ -0,0 +1,15 @@
+package Feedback1_2;
+
+public class TareaDeHiloLarga implements Runnable{
+    @Override
+    public void run() {
+        for (int i = 0; i < 20;i++) {
+            System.out.println(Thread.currentThread().getName());
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
